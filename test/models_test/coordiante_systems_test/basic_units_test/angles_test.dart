daa4aa458acb1bdf8d4306e498eadbff91bf7977
Nasser Amini <namini40@gmail.com>
2022-08-02 11:39:19 +0430

    test added: objects inits

--- test/models_test/coordiante_systems_test/basic_units_test/angles_test.dart
+++ test/models_test/coordiante_systems_test/basic_units_test/angles_test.dart
@@ -1,7 +1,26 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:na_map_tools/models/coordinate_systems/coordinate_system_files.dart';
 
 void main() {
-  group('testing Angle Units: object creation and APIs', () {});
+  double degreeRaw = 32.501245;
+  int dmsRawDeg = 35;
+  int dmsRawMinute = 12;
+  double dmsRawSecond = 45.2;
+  double radianRaw = 2.124587;
+
+  group('testing Angle Units: object creation and APIs', () {
+    final deg = AngleDegree(degreeRaw);
+    final dms = AngleDMS(dmsRawDeg, dmsRawMinute, dmsRawSecond);
+    final radian = AngleRadian(radianRaw);
+
+    test('if values match what they are given', () {
+      expect(deg.value, degreeRaw);
+      expect(dms.degree, dmsRawDeg);
+      expect(dms.minute, dmsRawMinute);
+      expect(dms.second, dmsRawSecond);
+      expect(radian.value, radianRaw);
+    });
+  });
 
   group('testing Angle Units: converting', () {});
 
