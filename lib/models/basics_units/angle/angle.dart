764698d593b32b182baeef6eb2d175ca27f2b477
Nasser Amini <namini40@gmail.com>
2022-08-29 14:56:00 +0430

    add DMS constructor to `Angle`

--- lib/models/basics_units/angle/angle.dart
+++ lib/models/basics_units/angle/angle.dart
@@ -1,10 +1,16 @@
 /// this is the base class for angular values
 /// for now its just based on degrees however it must be capable of radian and DMS and others
 class Angle {
-  double _value;
+  late double _value;
 
   // basic constructor
   Angle(double value) : _value = value;
+
+  // building object from Degree, minutes and seconds
+  Angle.fromDMS({int degree = 0, int minutes = 0, double seconds = 0.0})
+      : assert(minutes >= 60 && seconds >= 60) {
+    Angle(_dms2deg(degree, minutes, seconds));
+  }
 
   double get value => _value;
 
@@ -15,5 +21,9 @@ class Angle {
 
   Angle operator -(Angle other) {
     return Angle((_value - other.value) % 360.0);
+  }
+
+  double _dms2deg(int degree, int minutes, double seconds) {
+    return degree + minutes / 60 + seconds / 3600;
   }
 }
